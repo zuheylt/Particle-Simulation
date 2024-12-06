0eb994cf02b8f3b8a0dbc5dc09fae046c78f0c45
zuheyl <zuheyrtemel@gmail.com>
2024-12-06 16:44:45 +0300

    Introduced electronic loads and forces also particles loops over screen

--- particles.c
+++ particles.c
@@ -4,23 +4,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define RADIUS  3
-#define NUM_PARTICLES 28360                                                          
+#define RADIUS  4 // 1px = 1.325 pm  (1 pm = 10e-11 m )
+#define NUM_PARTICLES 50
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
 #define GAP_X (SCREEN_WIDTH/NUM_GRID_X)
 #define GAP_Y (SCREEN_HEIGHT/NUM_GRID_Y)
-#define NUM_GRID_X 8
-#define NUM_GRID_Y 3
+#define NUM_GRID_X 16
+#define NUM_GRID_Y 9
 #define GRID_NUMBER (NUM_GRID_X * NUM_GRID_Y)
+#define K_E 8.9875e5*5
 
 const int BASE_PARTICLES_PER_GRID = NUM_PARTICLES/GRID_NUMBER;
 const int REMAINDER_PARTICLES = NUM_PARTICLES % GRID_NUMBER;
-const int MAX_INITIAL_SPEED = 500;
+const double MAX_INITIAL_SPEED = 0; // *1.325/ms
+
 typedef struct {
-    float x, y;
-    float vx, vy;
-    float ax, ay;
+    double x, y;
+    double prev_x, prev_y;;
+    double ax, ay;
+    double mass;
+    double q;
+    double momentum_x;
+    double momentum_y;
 }Particle;
 
 typedef struct {
@@ -31,9 +37,10 @@ typedef struct {
 }Grid;
 
 
-float distance(Particle p1, Particle p2){
+double distance(Particle p1, Particle p2){
     return sqrtf((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
 }
+
 void initiate_grids(Grid grids[]) {    
     for(int l = 0; l < NUM_GRID_Y; l++) {
         for (int c = 0; c < NUM_GRID_X; c++) {
@@ -53,14 +60,56 @@ void initiate_grids(Grid grids[]) {
             }
         }
     }
-}                                                                                                                       
+}           
+
 void free_grids(Grid grids[]) {
     for (int i = 0; i < GRID_NUMBER; i++) {
         free(grids[i].particles);  
     }
 }
+
+void compute_forces(Particle particles[]){
+    for (int i =0; i < NUM_PARTICLES; i++){
+        particles[i].ax=particles[i].ay=0;
+        
+        for (int j=0; j< NUM_PARTICLES ; j++){
+            if (i!=j){
+                double dx =particles[i].x - particles[j].x;
+                double dy = particles[i].y - particles[j].y;
+                double r = distance(particles[i], particles[j]) + 1e-8;
+                
+                double force = K_E * particles[i].q * particles[j].q / (r*r);
+                double acc = force / particles[i].mass;
+
+                particles[i].ax += acc * dx / r;
+                particles[i].ay += acc * dy / r;
+
+
+            }
+        }
+        
+
+    }
+}
+
+
+
+void update_momentum (Particle *particle,Uint32 frame_time){
+    double dx = particle-> x - particle -> prev_x;
+    double dy = particle -> y - particle -> prev_y;
+
+    double vx = dx / frame_time;
+    double vy = dy / frame_time;
+
+    particle -> momentum_x = vx * particle -> mass;
+    particle -> momentum_y = vy * particle -> mass;
+
+}
+
+
 void initiate_particles(Particle particles[], Grid grids[]){
    int particle_index =0;
+
 
 
    for (int grid_idx=0; grid_idx <  GRID_NUMBER; grid_idx++){                                                                                                        
@@ -73,8 +122,8 @@ void initiate_particles(Particle particles[], Grid grids[]){
              int valid;
             do{
                 valid=1;
-                grids[grid_idx].particles[j].x = rand()%(GAP_X-(RADIUS)) + RADIUS + grids[grid_idx].x_origin;
-                grids[grid_idx].particles[j].y = rand()%(GAP_Y-(RADIUS)) + RADIUS + grids[grid_idx].y_origin;
+                grids[grid_idx].particles[j].prev_x = rand()%(GAP_X-(RADIUS)) + RADIUS + grids[grid_idx].x_origin;
+                grids[grid_idx].particles[j].prev_y = rand()%(GAP_Y-(RADIUS)) + RADIUS + grids[grid_idx].y_origin;
             
 
                 
@@ -85,35 +134,58 @@ void initiate_particles(Particle particles[], Grid grids[]){
                     }
                 }
             }while(!valid);
-            grids[grid_idx].particles[j].vx=((float)rand()/RAND_MAX) * MAX_INITIAL_SPEED - (MAX_INITIAL_SPEED/2);
-            grids[grid_idx].particles[j].vy=((float)rand()/RAND_MAX) * MAX_INITIAL_SPEED - (MAX_INITIAL_SPEED/2);
-            grids[grid_idx].particles[j].ax=0;
-            grids[grid_idx].particles[j].ay=0;
+            grids[grid_idx].particles[j].x=grids[grid_idx].particles[j].prev_x + 16 *(((float)rand()/RAND_MAX) * MAX_INITIAL_SPEED - (MAX_INITIAL_SPEED/2));
+            grids[grid_idx].particles[j].y=grids[grid_idx].particles[j].prev_y + 16 *(((float)rand()/RAND_MAX) * MAX_INITIAL_SPEED - (MAX_INITIAL_SPEED/2));
             particles[particle_index++]=grids[grid_idx].particles[j];
             
 
         }
 
-    }
-   
-    
+        for (int i = 0; i < NUM_PARTICLES; i ++){
+            particles[i].mass = 167e5;
+            particles[i].q =  (i % 2 == 0 ? 1:-1);
+            update_momentum(&particles[i], 16);
+            compute_forces(particles);
+        }
+    }    
 }
 
-void update_particles(Particle particles[], int num_particles, float dt){
-    for (int i=0;i<num_particles;i++){
-        if(particles[i].x<RADIUS||particles[i].x>SCREEN_WIDTH-RADIUS){
-            particles[i].vx *= -1;
+
+void update_particles(Particle particles[], int num_particles, Uint32 dt){
+    double total_momentum_x = 0;
+    double total_momentum_y = 0;
+    for (int i=0;i< num_particles;i++){
+        if(particles[i].x < -RADIUS || particles[i].x > SCREEN_WIDTH + RADIUS){
+            
+            double change_x = particles[i].x - particles[i].prev_x;
+            particles[i].x = SCREEN_WIDTH - particles[i].x;
+            particles[i].prev_x = particles[i].x - change_x;
+            
 
         }
-        if(particles[i].y<RADIUS||particles[i].y>SCREEN_HEIGHT-RADIUS){
-            particles[i].vy *= -1;
+        
+        if(particles[i].y<0 || particles[i].y>SCREEN_HEIGHT){
+            double change_y = particles[i].y - particles[i].prev_y;
+            particles[i].y = SCREEN_HEIGHT - particles[i].y;
+            particles[i].prev_y = particles[i].y - change_y;
 
         }
+    
+        compute_forces(particles);
 
+        double temp_x = particles[i].x;
+        double temp_y = particles[i].y;
 
-        particles[i].x += particles[i].vx*dt;
-        particles[i].y += particles[i].vy*dt;
+        particles[i].x = 2 * particles[i].x - particles[i].prev_x + particles[i].ax * dt;
+        particles[i].y = 2 * particles[i].y - particles[i].prev_y + particles[i].ay * dt;
+
+        particles[i].prev_x = temp_x;
+        particles[i].prev_y = temp_y;
+        update_momentum(&particles[i],dt);
+        total_momentum_x += particles[i].momentum_x;
+        total_momentum_y += particles[i].momentum_y;
     }
+    printf("total momentum over x: %.14f\n", total_momentum_x);
 }
 
 void draw_circle(SDL_Renderer *renderer, Particle particles[]){
@@ -187,7 +259,7 @@ int main(){
     initiate_particles(particles,grids);
     printf("particles initiated\n");
 
-
+    Uint32 frame_time = 16;
     int running =1;
     Uint64 start_time= SDL_GetPerformanceCounter();
     Uint64 frequency = SDL_GetPerformanceFrequency();
@@ -201,7 +273,7 @@ int main(){
             }
         }
         Uint32 frame_start = SDL_GetPerformanceCounter();
-        update_particles(particles,NUM_PARTICLES,0.016);
+        update_particles(particles,NUM_PARTICLES,frame_time);
 
         SDL_SetRenderDrawColor(renderer,0,0,0,255);
         SDL_RenderClear(renderer);
@@ -222,11 +294,9 @@ int main(){
         SDL_RenderPresent(renderer);
 
         Uint32 frame_end = SDL_GetPerformanceCounter();
-        Uint32 frame_time = (Uint32)((frame_end - frame_start) * 1000 / frequency);
+        frame_time = (Uint32)((frame_end - frame_start) * 1000 / frequency);
         frame_count++;
-        if(frame_time<16){
-            SDL_Delay(16-frame_time);
-        }
+        
 
         Uint64 currentTime = SDL_GetPerformanceCounter();
         double passed_time = (currentTime-start_time)/(double)frequency;
